d35fb78eb556eef28e30f19c849fb2bcfb026d46
Onkar Dhanaji Patil <148969149+Omkrishna-git@users.noreply.github.com>
2025-05-07 18:23:40 +0530

    Update FinalNQ.cpp

--- Assig_4/FinalNQ.cpp
+++ Assig_4/FinalNQ.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include <cmath>
 using namespace std;
-  
+   
 // ---------------------------- Backtracking Class ----------------------------
 class NQueensBackTracking {
 private:
